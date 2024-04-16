3997355bb6d14d63b4fe9627350bb181287f4183
anthony-mini <anthony-mini@outlook.fr>
2024-04-16 14:21:31 +0200

    Mise à jour de package.json et index.ts, ajout de tsconfig.json et backend/tsconfig.json, mise à jour de .eslintrc.json, et main.ts.

--- backend/index.ts
+++ backend/index.ts
@@ -1,1 +1,23 @@
-console.log('Hello, world bébouuu!');
+import { Server } from 'socket.io';
+
+function main() {
+  const io = new Server({
+    cors: {
+      origin: '*',
+    },
+  });
+
+  io.on('connection', (socket) => {
+    console.log('New conneciton', socket.id);
+    socket.on('message', (message) => {
+      console.log('A user send message', message);
+      io.emit('message', message);
+    });
+  });
+
+  io.listen(3000);
+
+  console.log('Server is running on port 3000');
+}
+
+main();
